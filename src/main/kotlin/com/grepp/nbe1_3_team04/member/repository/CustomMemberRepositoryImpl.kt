357c0e9b969ae98451c8fdbe5d82700cff4d9130
MoonJaeGyeong <bblbblan69@gmail.com>
2024-10-25 11:28:08 +0900

    memberRepository 마이그레이션

---
+++ src/main/kotlin/com/grepp/nbe1_3_team04/member/repository/CustomMemberRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.grepp.nbe1_3_team04.member.repository
+
+import com.grepp.nbe1_3_team04.global.domain.IsDeleted
+import com.grepp.nbe1_3_team04.member.domain.QMember.member
+import com.querydsl.jpa.impl.JPAQueryFactory
+
+class CustomMemberRepositoryImpl(private val queryFactory: JPAQueryFactory) : CustomMemberRepository {
+
+
+    override fun findMemberIdByMemberEmail(email: String?): Long? {
+        return queryFactory.select(member.memberId)
+            .from(member)
+            .where(member.email.eq(email)
+                    .and(member.isDeleted.eq(IsDeleted.FALSE)))
+            .fetchOne()
+    }
+
+    override fun existByEmail(email: String?): Boolean {
+        val count = queryFactory
+            .selectOne()
+            .from(member)
+            .where(
+                member.email.eq(email)
+                    .and(member.isDeleted.eq(IsDeleted.FALSE))
+            )
+            .fetchFirst()
+
+        return count != null
+    }
+}
