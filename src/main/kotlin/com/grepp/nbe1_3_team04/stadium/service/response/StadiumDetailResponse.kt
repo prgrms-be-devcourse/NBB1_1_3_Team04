de286aace8f6194e9b386492889edc967a940858
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-10-27 14:50:56 +0900

    feat: #27 StadiumResponse 마이그레이션

---
+++ src/main/kotlin/com/grepp/nbe1_3_team04/stadium/service/response/StadiumDetailResponse.kt
@@ -0,0 +1,29 @@
+package com.grepp.nbe1_3_team04.stadium.service.response
+
+import com.grepp.nbe1_3_team04.stadium.domain.Stadium
+
+data class StadiumDetailResponse(
+    val stadiumId: Long,
+    val memberId: Long,
+    val name: String,
+    val address: String,
+    val phoneNumber: String,
+    val description: String?,
+    val latitude: Double,
+    val longitude: Double
+) {
+    companion object {
+        fun from(stadium: Stadium): StadiumDetailResponse {
+            return StadiumDetailResponse(
+                stadiumId = requireNotNull(stadium.stadiumId),
+                memberId = requireNotNull(stadium.member.memberId),
+                name = stadium.name,
+                address = stadium.address,
+                phoneNumber = stadium.phoneNumber,
+                description = stadium.description,
+                latitude = stadium.position.latitude,
+                longitude = stadium.position.longitude
+            )
+        }
+    }
+}
