7c07f7a14506c538792b17bd3bbba0a7638934b3
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-10-27 14:58:04 +0900

    feat: #27 Court 커스텀 어노테이션 마이그레이션

---
+++ src/main/kotlin/com/grepp/nbe1_3_team04/stadium/api/request/validation/CourtAllowedValues.kt
@@ -0,0 +1,14 @@
+package com.grepp.nbe1_3_team04.stadium.api.request.validation
+
+import jakarta.validation.Constraint
+import jakarta.validation.Payload
+import kotlin.reflect.KClass
+
+@Constraint(validatedBy = [CourtAllowedValuesValidator::class])
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class CourtAllowedValues(
+    val message: String = "유효하지 않은 값입니다.",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
