edf49a35ce475f66e41f21448f3537ab03587ebe
MoonJaeGyeong <bblbblan69@gmail.com>
2024-10-29 17:00:47 +0900

    feat : SchedulingConfig 마이그레이션

---
+++ src/main/kotlin/com/grepp/nbe1_3_team04/config/SchedulingConfig.kt
@@ -0,0 +1,24 @@
+package com.grepp.nbe1_3_team04.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+
+@EnableScheduling
+@Configuration
+class SchedulingConfig {
+    @Bean
+    fun taskScheduler(): ThreadPoolTaskScheduler {
+        val taskScheduler = ThreadPoolTaskScheduler()
+        taskScheduler.poolSize = POOL_SIZE
+        taskScheduler.setThreadNamePrefix(THREAD_NAME_PREFIX)
+        taskScheduler.initialize()
+        return taskScheduler
+    }
+
+    companion object {
+        const val POOL_SIZE: Int = 10
+        const val THREAD_NAME_PREFIX: String = "TaskScheduler-"
+    }
+}
