27a1a45d24b6a8ffe597d7bafe758bd19a3fff79
MoonJaeGyeong <bblbblan69@gmail.com>
2024-10-29 11:35:34 +0900

    feat : validator 마이그레이션

---
+++ src/main/kotlin/com/grepp/nbe1_3_team04/member/api/request/validation/PasswordMatches.kt
@@ -0,0 +1,16 @@
+package com.grepp.nbe1_3_team04.member.api.request.validation
+
+import jakarta.validation.Constraint
+import jakarta.validation.Payload
+import kotlin.reflect.KClass
+
+@Constraint(validatedBy = [PasswordMatchesValidator::class])
+@Target(AnnotationTarget.CLASS) // 클래스 수준에서 검증
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PasswordMatches(
+    val message: String = "패스워드가 일치하지 않습니다.",
+    val passwordField: String,
+    val passwordConfirmField: String,
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Any>> = []
+)
---
+++ src/main/kotlin/com/grepp/nbe1_3_team04/member/api/request/validation/PasswordMatchesValidator.kt
@@ -0,0 +1,36 @@
+package com.grepp.nbe1_3_team04.member.api.request.validation
+
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+
+class PasswordMatchesValidator : ConstraintValidator<PasswordMatches, Any> {
+    private var passwordField: String? = null
+    private var passwordConfirmField: String? = null
+
+    override fun initialize(constraintAnnotation: PasswordMatches) {
+        this.passwordField = constraintAnnotation.passwordField
+        this.passwordConfirmField = constraintAnnotation.passwordConfirmField
+    }
+
+    override fun isValid(value: Any, constraintValidatorContext: ConstraintValidatorContext): Boolean {
+        try {
+            val password = value.javaClass.getDeclaredField(passwordField)
+            val passwordConfirm = value.javaClass.getDeclaredField(passwordConfirmField)
+
+            password.isAccessible = true
+            passwordConfirm.isAccessible = true
+
+            val passwordValue = password[value] as String?
+            val passwordConfirmValue = passwordConfirm[value] as String?
+
+            if (passwordValue == null && passwordConfirmValue == null) { // OAuth 2.0 로그인 일 시
+                return true
+            }
+
+            return passwordValue == passwordConfirmValue
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
+    }
+}
