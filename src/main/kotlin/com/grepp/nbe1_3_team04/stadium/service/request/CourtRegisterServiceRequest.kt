e52435c015a89d9f303e4077a99037deb1223b05
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-10-29 15:58:35 +0900

    refactor: #28 카멜 케이스 준수

--- src/main/kotlin/com/grepp/nbe1_3_team04/stadium/service/request/CourtRegisterServiceRequest.kt
+++ src/main/kotlin/com/grepp/nbe1_3_team04/stadium/service/request/CourtRegisterServiceRequest.kt
@@ -6,5 +6,5 @@ data class CourtRegisterServiceRequest(
     val stadiumId: Long,
     val name: String,
     val description: String?,
-    val price_per_hour: BigDecimal
+    val pricePerHour: BigDecimal
 )
