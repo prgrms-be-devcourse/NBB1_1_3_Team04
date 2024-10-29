845a4db13968e87708e369200d649344a1b9352d
HMWG <nurung22222@gmail.com>
2024-10-29 17:48:29 +0900

    feat: #46 채팅 관련 서비스 인터페이스 마이그레이션

---
+++ src/main/kotlin/com/grepp/nbe1_3_team04/chat/service/ChatroomService.kt
@@ -0,0 +1,22 @@
+package com.grepp.nbe1_3_team04.chat.service
+
+import com.grepp.nbe1_3_team04.chat.service.request.ChatroomServiceRequest
+import com.grepp.nbe1_3_team04.chat.service.response.ChatroomResponse
+import org.springframework.stereotype.Service
+
+@Service
+interface ChatroomService {
+    fun createChatroom(request: ChatroomServiceRequest): ChatroomResponse
+
+    fun deleteChatroomByChatroomId(chatroomId: Long): Long
+
+    fun updateChatroom(chatroomId: Long, request: ChatroomServiceRequest): ChatroomResponse
+
+    fun createReservationChatroom(request: ChatroomServiceRequest, reservationId: Long): ChatroomResponse
+
+    fun createTeamChatroom(request: ChatroomServiceRequest, teamId: Long): ChatroomResponse
+
+    fun deleteTeamChatroom(teamId: Long): Long
+
+    fun deleteReservationChatroom(reservationId: Long): Long
+}
