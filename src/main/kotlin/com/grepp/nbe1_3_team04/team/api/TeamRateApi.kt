35d9d5a4a5a45ddffc3bd0d2ae13a1ea830cb6d4
pyy2114 <pyy2114@naver.com>
2024-10-30 10:43:56 +0900

    feat: #54 팀 컨트롤러 마이그레이션

---
+++ src/main/kotlin/com/grepp/nbe1_3_team04/team/api/TeamRateApi.kt
@@ -0,0 +1,9 @@
+package com.grepp.nbe1_3_team04.team.api
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping
+class TeamRateApi 
